b25de0d42ed6c3b1f54152166f7b502f044d55be
Junyang Chen <junyang@Junyangs-MacBook-Pro.local>
2015-10-20 11:23:05 -0400

    format the code

--- cachematrix.R
+++ cachematrix.R
@@ -15,9 +15,10 @@ makeCacheMatrix <- function(x = matrix()) {
     get <- function() x
     setinverse <- function(inverse)  inv <<- inverse
     getinverse <- function() inv
+    
     list(set = set, get = get,
-    setinverse = setinverse,
-    getinverse = getinverse)
+      setinverse = setinverse,
+      getinverse = getinverse)
 }
 
 
